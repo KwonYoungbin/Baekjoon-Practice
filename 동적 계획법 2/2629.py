a837c31a8dbd4d275af792d012c1b6dd3beef727
Kwon Youngbin <kyb951020@naver.com>
2022-01-05 16:44:41 +0900

    양팔저울 _ DP 문제 유형 중 냅색 알고리즘 응용 및 변형 문제, 05 Jan 2022

---
+++ 동적 계획법 2/2629.py
@@ -0,0 +1,31 @@
+def scale(weights, n, now, left, right, dp, set_arr):
+    val = abs(left - right)
+    set_arr.add(val)
+    
+    if now == n:
+        return 0
+    
+    if dp[now][val] == 0:
+        # 왼쪽 저울에 올릴 때
+        scale(weights, n, now+1, left+weights[now], right, dp, set_arr)
+        
+        # 오른쪽 저울에 올릴 때
+        scale(weights, n, now+1, left, right+weights[now], dp, set_arr)
+        
+        # 저울에 올리지 않을 때
+        scale(weights, n, now+1, left, right, dp, set_arr)
+        
+        dp[now][val] = 1
+    
+
+n = int(input())
+weights = list(map(int, input().split()))
+chk_cnt = int(input())
+chk_list = list(map(int, input().split()))
+dp = [[0]*15001 for _ in range(n+1)]
+set_arr = set()
+
+scale(weights, n, 0, 0, 0, dp, set_arr)
+
+for number in chk_list:
+    print('Y', end=' ') if number in set_arr else print('N', end=' ')
