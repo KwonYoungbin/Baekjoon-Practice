417eb0ddfb362ec22a379b00d8d78ce2a2b539c6
Kwon Youngbin <kyb951020@naver.com>
2022-01-10 11:42:49 +0900

    윷놀이 _ 브론즈 3 기본 문제, 10 Jan 2022

--- 등급별 풀이/브론즈/브론즈_3/2490.py
+++ 등급별 풀이/브론즈/브론즈_3/2490.py
@@ -1,13 +1,13 @@
 arr = [list(map(int, input().split())) for _ in range(3)]
 
 for sub in arr:
-    if sub.count(1) == 1:
+    if sub.count(0) == 1:
         print('A')
-    elif sub.count(1) == 2:
+    elif sub.count(0) == 2:
         print('B')
-    elif sub.count(1) == 3:
+    elif sub.count(0) == 3:
         print('C')
-    elif sub.count(1) == 4:
+    elif sub.count(0) == 4:
         print('D')
     else:
         print('E')
