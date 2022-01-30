a738ebc82aef405bb4637a818c98ef77cb2f359a
Kwon Youngbin <kyb951020@naver.com>
2022-01-30 15:04:11 +0900

    다음 순열 _ 실버 3 원리를 떠올리기 어려운 문제, 30 Jan 2022

---
+++ 등급별 풀이/실버/실버_3/10972.py
@@ -0,0 +1,19 @@
+N = int(input())
+arr = list(map(int, input().split()))
+
+possible = False
+for i in range(N-1, 0, -1):
+    if arr[i-1] < arr[i]:
+        idx = i-1
+        possible = True
+        break
+
+if not possible:
+    print(-1)
+else:    
+    for i in range(N-1, 0, -1):
+        if arr[idx] < arr[i]:
+            arr[idx], arr[i] = arr[i], arr[idx]
+            arr = arr[:idx+1] + sorted(arr[idx+1:])
+            print(*arr)
+            break
