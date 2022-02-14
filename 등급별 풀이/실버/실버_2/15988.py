134dc44b3255a1adc657f91ba9fb61737110b165
Kwon Youngbin <kyb951020@naver.com>
2022-02-14 14:37:55 +0900

    1, 2, 3 더하기 3 _ 실버 2 DP 기본 문제, 14 Feb 2022

---
+++ 등급별 풀이/실버/실버_2/15988.py
@@ -0,0 +1,10 @@
+dp = [0] * 1000001
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+for i in range(4, 1000001):
+    dp[i] = (dp[i-1]+dp[i-2]+dp[i-3]) % 1000000009
+    
+for _ in range(int(input())):
+    print(dp[int(input())])
